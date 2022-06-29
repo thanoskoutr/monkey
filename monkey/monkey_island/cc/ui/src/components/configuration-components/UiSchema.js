22c640702b74de6eb5ca944a55e85df0e7b5251b
Ilija Lazoroski <ilija.la@live.com>
2022-06-29 18:32:48 +0200

    UI: Add propagation to the UI schema

--- monkey/monkey_island/cc/ui/src/components/configuration-components/UiSchema.js
+++ monkey/monkey_island/cc/ui/src/components/configuration-components/UiSchema.js
@@ -7,6 +7,24 @@ import SensitiveTextInput from '../ui-components/SensitiveTextInput';
 
 export default function UiSchema(props) {
   const UiSchema = {
+    propagation: {
+      'ui:order': ['exploitation', 'maximum_depth', 'network_scan'],
+      exploitation: {
+        brute_force: {
+          'ui:widget': AdvancedMultiSelect
+        },
+        vulnerability: {
+          'ui:widget': AdvancedMultiSelect
+        }
+      },
+      network_scan: {
+        targets: {
+          info_box: {
+            'ui:field': InfoBox
+          }
+        }
+      }
+    },
     basic: {
       'ui:order': ['exploiters', 'credentials'],
       exploiters: {
